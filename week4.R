c4aae5af0ffd8cf0e951ca265e45217f8e753aba
swmpkim <kimberly.cressman@dmr.ms.gov>
2018-04-23 14:41:44 -0500

    added graph of fully overlapping bars
    suggested by Thomas Mock via Twitter

--- week4.R
+++ week4.R
@@ -41,3 +41,27 @@ ggplot(dat_top_both) +
     coord_flip()
 
 
+
+# for overlapping bars:
+# spread and put in order by women's salaries;
+dat_top_overlap <- dat %>%
+    filter(occupation %in% dat_top_women$occupation) %>%
+    select(occupation, gender, average_taxable_income) %>%
+    spread(key = gender, value = average_taxable_income) %>%
+    mutate(occupation = fct_reorder(occupation, Female, .desc = FALSE))
+    
+
+# plot it
+ggplot(dat_top_overlap) +
+    geom_col(aes(x = occupation, y = Male/1000, fill = "Male"),
+             col = "gray50", width = 0.8) +
+    geom_col(aes(x = occupation, y = Female/1000, fill = "Female"), 
+             col = "gray50", width = 0.5) +
+    ggtitle("Gender differences in income in Australia", 
+            subtitle = "10 top-paying professions for women") +
+    xlab("Occupation") +
+    ylab("Mean salary \n(thousands of dollars)") +
+    scale_fill_manual(name = "gender",
+                      values = c("Female" = "cadetblue3", "Male" = "gray80")) +
+    theme_minimal() +
+    coord_flip()
