d45e4f25390c8554500df92593cb5af1fa66e69d
swmpkim <kimberly.cressman@dmr.ms.gov>
2018-06-27 16:24:40 -0500

    submitted for week 13

--- R/week13.R
+++ R/week13.R
@@ -8,26 +8,37 @@ dat <- read.csv("data/week13_alcohol_global.csv")
 skim(dat)
 head(dat)
 
-# reshape for faceted plotting, because i'm lazy and want to make just one plot
 
+# reshape so I can group and make faceted plots
+# top_n to pull out 40 countries with highest consumption
+# make country a factor
+# gather() to put it in long format
 dat2 <- dat %>%
     rename(beer = beer_servings,
            wine = wine_servings,
            spirits = spirit_servings) %>%
-    top_n(40, total_litres_of_pure_alcohol) %>%
-    mutate(country = as.factor(country)) %>%
+    top_n(40, total_litres_of_pure_alcohol) %>%  
+    mutate(country = as.factor(country)) %>%  
     gather(key = "alc_type", value = "value", 
            -country, -total_litres_of_pure_alcohol) 
-head(dat2)
 
 
-plot(dat)
+# scatterplot matrix
+# (everything against everything else)
+plot(dat, main = "Scatterplot Matrix")
 
+
+# exploratory bar chart
 ggplot(dat2, aes(x = alc_type, y = value, fill = alc_type)) +
-    geom_col() +
+    geom_col(show.legend = FALSE) +
     facet_wrap(~country, ncol = 5) +
     scale_fill_brewer(type = "qual", palette = "Paired") +
-    theme_bw()
+    theme_bw() +
+    labs(title = "Amount of alcohol consumed by type", 
+         subtitle = "40 countries with highest total litres of pure alcohol", 
+         x = "alcohol type", 
+         y = "# servings")
+
 
 # hm, how many countries have beer as top? spirits? wine?
 # order within each country, and rank?
